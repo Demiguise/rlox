41a4093e7c2efb839cf2e31660cb53c27d982ee4
Andrew Hewitson <andy.c.hewitson@gmail.com>
2023-01-14 13:59:05 +0100

    Add basic structure

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::{env, fs};
+use text_io::read;
+
+fn run(input: String) {
+    println!("{}", input)
+}
+
+fn run_file(file_path: &String) {
+    println!("Attempting to load file [{}]", file_path);
+    let contents = fs::read_to_string(file_path).expect("Unable to read file");
+    run(contents)
+}
+
+fn run_prompt() {
+    println!("Running Interpreter");
+    loop {
+        print!("> ");
+        let line: String = read!("{}\n");
+        run(line)
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => run_prompt(),
+        2 => run_file(&args[1]),
+        _ => println!("Wrong usage, try again"),
+    }
+}
